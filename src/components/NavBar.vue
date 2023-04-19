6aa8fec7284bd048baa88a51f26c91580e337f6b
r11725025 <r11725025@ntu.edu.tw>
2023-04-19 20:26:42 +0800

    add matching room init, matching room profile

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,90 +1,143 @@
 <template>
-    
-        <v-app>
 
-            <v-navigation-drawer color="black">
-              <img src="../assets/logo.png">
-              <!-- < v-on:click="redirect_to_login" /> -->
-            </v-navigation-drawer>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="../css/style.css"/>
+  <!-- <div v-is='script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"'></div> -->
 
-            <v-app-bar color="black">
-              
-              <v-responsive max-width="400">
-                <!-- <v-text-field
-                  :label="$t('search')"
-                  solo
-                  dense
-                  flat
-                  hide-details
-                  rounded
-                  append-outer-icon="search"
-                >
-              </v-text-field> -->
-              <SearchBar/>
+    <v-app-bar color="black">
+      <a href="/" style="text-decoration:none;">
+          <font size="6" color="white" face="Arial Black">Teamatch</font>
+      </a>
+     
+     
+          <nav class="navbar navbar-expand-lg">
+              <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <form class="d-flex">
+                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                      <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                  </ul>
+                  
+                </div>
                 
-        </v-responsive>
-        <v-spacer></v-spacer>
-                <ChangeLang />
-            </v-app-bar>
 
-            <!-- 根据应用组件来调整你的内容 -->
-            <v-main>
-              <h1>{{ $t('search') }}</h1>
-              <h1>{{ $t('greeting') }}</h1>
-              <h1>{{ $t('greeting') }}</h1>
-              <h1>{{ $t('greeting') }}</h1>
-              <h1>{{ $t('greeting') }}</h1>
-              <h1>{{ $t('search') }}</h1>
-              <h1>{{ $t('greeting') }}</h1>
-              <h1>{{ $t('greeting') }}</h1>
-              <h1>{{ $t('greeting') }}</h1>
-              <h1>{{ $t('greeting') }}</h1>
-              <h1>{{ $t('search') }}</h1>
-              <h1>{{ $t('greeting') }}</h1>
-              <h1>{{ $t('greeting') }}</h1>
-              <h1>{{ $t('greeting') }}</h1>
-              <h1>{{ $t('greeting') }}</h1>
+              </div>
               
-                <!-- 给应用提供合适的间距 -->
-                <v-container >
+                  
+                    <a href="#" class="text-decoration-none text-white"><i class="bi bi-bell-fill"></i></a>
+                  
+                  
+                    <a href="#" class="text-decoration-none text-white"><i class="bi bi-person-circle"></i></a>
+                  
+              
+            </nav>
 
-                <!-- 如果使用 vue-router -->
-                    <router-view></router-view>
-                </v-container>
-            </v-main>
+            <!-- <div id="app">
+              <route-view></route-view>
+            </div> -->
+    </v-app-bar>
+    <v-navigation-drawer color="black">
+      
+      <v-list>
+        <button href="#" class="btn btn-primary">新增配對空間</button>
+      </v-list>
+      <v-list>
+        <a href="#" class="text-decoration-none text-white"><i class="bi bi-people-fill"></i>  所有配對空間</a>
+      </v-list>
+      <v-list>
+        <a href="#" class="text-decoration-none text-white"><i class="bi bi-person-fill"></i>  我的配對空間</a>
+      </v-list>
+      <v-list>
+        <a href="#" class="text-decoration-none text-white"><i class="bi bi-chat-fill"></i>  我的小組</a>
+      </v-list>
+      <v-list>
+        <hr class="h-color mx-2">
+      </v-list>
+      <v-list>
+        <ChangeLang />
+      </v-list>
+    </v-navigation-drawer>
 
-            <v-footer>
-            <!-- -->
-            </v-footer>
-            
-            
-       
-        
-    </v-app>
-    
-  </template>
-  
-  <script>
-  import ChangeLang from "./ChangeLang.vue";
-  import SearchBar from "./SearchBar.vue";
-  
-  export default {
-    components: {
-      ChangeLang,
-      SearchBar
+</template>
+
+<script>
+import ChangeLang from "./ChangeLang.vue";
+import SearchBar from "./SearchBar.vue";
+
+export default {
+components: {
+  ChangeLang,
+  SearchBar
+}
+};
+</script>
+
+<!-- <script>
+import { mapGetters } from "vuex";
+import { GetProductsCategories } from "@/api/home";
+export default {
+  data() {
+    return {
+      categories: []
+    };
+  },
+  computed: {
+    ...mapGetters({ isLogin: "login/IsLogin" }),
+    ...mapGetters({ CartItemLength: "shoppingCart/CartItemLength" }),
+    ...mapGetters({ IsSeller: "login/IsSeller" }),
+    ...mapGetters({ moneyInfo: "wallet/GetWallet" }),
+    getUrl: () =>
+      function(category) {
+        return `/home?category=${category}`;
+      }
+  },
+  async created() {
+    await this.$store.dispatch("wallet/init");
+    this.categories = (await GetProductsCategories()).categories;
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch("login/logout");
+      this.$router.push("/home").catch(err => {
+        err;
+      });
+    },
+    titleCase(str) {
+      const newStr = str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+      return newStr;
     }
-  };
-  </script>
-  
-  
-  <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
   }
-  </style>
-  
+};
+</script> -->
+
+<!-- <style>
+body{
+  background: #eee;
+}
+
+#side_nav{
+  background:'black';
+  min-width: 250px;
+  max-width: 250px;
+}
+
+.content{
+  min-height: 100vh;
+  width: 100%;
+}
+
+hr.h-color{
+  border: 0;
+  background: #eee;
+  height: 3px;
+}
+
+
+.sidebar li a{
+  color:#fff;
+}
+
+</style>
+ -->
--- src/view/Home1.vue
+++ src/view/Home1.vue
@@ -1,91 +1,81 @@
-<script src="https://kit.fontawesome.com/68eab83108.js" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.3.4/axios.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-<script src="https://unpkg.com/vue@3"></script>
-<script src="https://unpkg.com/vue-router@4"></script>
-<script src="app.js"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 <template>
-  <!-- <div v-is='script src="https://kit.fontawesome.com/68eab83108.js" crossorigin="anonymous"'></div>
-  <div v-is=''></div>
-  <div v-is=''></div>
-  <div v-is='script src="https://kit.fontawesome.com/68eab83108.js" crossorigin="anonymous"'></div>
-  <div v-is='script src="https://kit.fontawesome.com/68eab83108.js" crossorigin="anonymous"'></div>
-  <div v-is='script src="https://kit.fontawesome.com/68eab83108.js" crossorigin="anonymous"'></div>
-  <div v-is='script src="https://kit.fontawesome.com/68eab83108.js" crossorigin="anonymous"'></div>
-  <div v-is='script src="https://kit.fontawesome.com/68eab83108.js" crossorigin="anonymous"'></div> -->
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/style.css"/>
     
+  <v-app>
+      
+  
 
-</head>
-
-<body>
-
-    <div  class="main-container d-flex">
-        <div class="sidebar" id="side_nav">
-            <div class="header-box px-2 pt-3 pb-4">
-                <h1 class="fs-4"><span class="text-white px-2 me-2">Teamatch</span></h1>
-                <button class="btn d-md-none d-block close-btn px-1 py-0 text-white"><i class="fas fa-stream"></i></button>
-            </div>
-
-            <ul class="list-unstyled px-2">
-                <li class=""><button href="#" class="btn btn-primary">新增配對空間</button></li>
-                <li class="" style="padding-bottom: 10px; padding-top: 20px;"><a href="#" class="text-decoration-none text-white"><i class="fa-solid fa-users" style="color: #ffffff;"></i>  所有配對空間</a></li>
-                <li class="" style="padding-bottom: 10px;"><a href="#" class="text-decoration-none"><i class="fa-solid fa-user" style="color: #ffffff;"></i>  我的配對空間</a></li>
-                <li class=""><a href="#" class="text-decoration-none"><i class="fa-solid fa-comment" style="color: #ffffff;"></i>  我的小組</a></li>
-            </ul>
-            <hr class="h-color mx-2">
-
-        </div>
-        <div class="content">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-            
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                      <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                      </form>
-                    </ul>
-                    
-                  </div>
-                  <ul class="navbar-nav">
-                    <li class="nav-item">
-                      <a href="#"><i class="fa-solid fa-bell fa-2xl"></i></a>
-                    </li>
-                    <li class="nav-item">
-                      <a href="#"><i class="fa-solid fa-circle-user fa-2xl"></i></a>
-                    </li>
-                  </ul>
-
-                </div>
-              </nav>
-
-              <div id="app">
-                <route-view></route-view>
-              </div>
-
-        </div>
-
-
-        
-            
+      <!-- 改這個地方!!!!!! -->
+      <v-main>
+        <h1>HOME的啦</h1>
+        <div class="d-flex justify-content-center"
+              v-for="item in matching_rooms">
+              
+          <h1>{{ item }}</h1>
+              
         </div>
         
+          <!-- 给应用提供合适的间距 -->
+          <v-container >
+
+          <!-- 如果使用 vue-router -->
+              <router-view></router-view>
+          </v-container>
+      </v-main>
+
+      <v-footer>
+      <!-- -->
+      </v-footer>
+      
+      
  
+  
+</v-app>
 
-    
-    
-    
-
-    
-
-</body>
 </template>
+
+<script>
+import ChangeLang from "../components/ChangeLang.vue";
+import SearchBar from "../components/SearchBar.vue";
+import axios from "axios";
+
+export default {
+data() {
+  return {
+    matching_rooms: []
+  };
+},
+components: {
+ChangeLang,
+SearchBar
+},
+methods: {
+refresh_mr(){
+  axios.post('http://localhost:8000/api/v1/search/matching-room/list',{
+    user_email: "",
+    prompt: "",
+    query_all: true
+})
+  .then( (mr_response) => {this.matching_rooms = mr_response.data.data.map(aaa => aaa.name);
+      console.log(this.matching_rooms);})
+  .catch( (error) => console.log(error));
+     
+   
+  }
+},
+mounted:function(){
+  this.refresh_mr();
+}
+};
+</script>
+
+
+<style>
+#app {
+font-family: Avenir, Helvetica, Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+text-align: center;
+color: #2c3e50;
+margin-top: 60px;
+}
+</style>
